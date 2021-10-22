59f10ec1ed4bd59a662da4f1eaad099978fc563e
Meissa <meissa.benali@esprit.tn>
2021-10-22 13:30:12 +0200

    Commit 1

---
+++ src/main/java/tn/esprit/spring/repository/DepartementRepository.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import tn.esprit.spring.entity.Departement;
+
+public interface DepartementRepository extends CrudRepository<Departement, Long>
+{
+	
+	
+
+}
