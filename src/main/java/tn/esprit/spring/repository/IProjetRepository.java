dc4c72245ce3ff0dcd04d6b77fe9e4e057669b77
rachedchakchouk <rached.chakchouk@esprit.tn>
2021-11-02 01:33:20 +0100

    version1.0

--- src/main/java/tn/esprit/spring/repository/IProjetRepository.java
+++ src/main/java/tn/esprit/spring/repository/IProjetRepository.java
@@ -1,9 +1,12 @@
 package tn.esprit.spring.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entity.Projet;
 
+
+@Repository
 public interface IProjetRepository extends CrudRepository<Projet, Integer> {
 
 }
