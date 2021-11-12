2992e66c7571784bbe9e778b21083ba176daa878
TarekMESSAOUDI <tarek.messaoudi@esprit.tn>
2021-11-12 15:25:13 +0100

    Jenkinsfile update

--- src/test/java/tn/esprit/spring/TimesheetDevOpsApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetDevOpsApplicationTests.java
@@ -85,7 +85,7 @@ public class TimesheetDevOpsApplicationTests {
 	public void testDeleteEmploye() {
 		Employe emp = new Employe();
 		assertNotNull(emp);
-		er.deleteById((long) 54);
+		er.deleteById((long) 56);
 		l.info("Employe deleted successfuly" );
 	}
 }
